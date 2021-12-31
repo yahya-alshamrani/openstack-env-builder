9b47bc4af13e1705246cac392f146235f49c5a2c
yahya-alshamrani <93801490+yahya-alshamrani@users.noreply.github.com>
2021-12-31 20:31:58 +0300

    Add allow-icmp to the instance creation command

--- script.sh
+++ script.sh
@@ -28,13 +28,20 @@ source admin
 openstack project create hr --description 'HR Department'
 openstack user create --project hr --password 'password' --email 'hr-user@localhost' hr-user
 openstack role add --project hr --user hr-user _member_
+
 openstack flavor create --id 6 --ram 256 --disk 1 --vcpus 1 --public m1.mini
 openstack flavor create --id 7 --ram 128 --disk 1 --vcpus 1 --public m1.micro
-curl -L https://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img | openstack image create --container-format bare --disk-format qcow2 --public 'cirros-0.3.5'
-curl -L https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img | openstack image create --container-format bare --disk-format qcow2 --public 'cirros-0.4.0'
-curl -L https://download.cirros-cloud.net/0.5.2/cirros-0.5.2-x86_64-disk.img | openstack image create --container-format bare --disk-format qcow2 --public 'cirros-0.5.2'
+
+curl -L https://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img | openstack image create \
+--container-format bare --disk-format qcow2 --public 'cirros-0.3.5'
+curl -L https://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img | openstack image create \
+--container-format bare --disk-format qcow2 --public 'cirros-0.4.0'
+curl -L https://download.cirros-cloud.net/0.5.2/cirros-0.5.2-x86_64-disk.img | openstack image create \
+--container-format bare --disk-format qcow2 --public 'cirros-0.5.2'
+
 openstack network create --share --external --provider-network-type flat --provider-physical-network extnet public
-openstack subnet create --subnet-range '10.10.10.0/24' --no-dhcp --gateway '10.10.10.1' --network public --allocation-pool start=10.10.10.100,end=10.10.10.200 --dns-nameserver 8.8.8.8 public-subnet
+openstack subnet create --subnet-range '10.10.10.0/24' --no-dhcp --gateway '10.10.10.1' \
+--network public --allocation-pool start=10.10.10.100,end=10.10.10.200 --dns-nameserver 8.8.8.8 public-subnet
 
 source hr-user
 openstack network create hr-network-1
@@ -47,7 +54,8 @@ openstack security group rule create --dst-port 22:22 --protocol tcp --ingress '
 openstack security group create 'allow-icmp'
 openstack security group rule create --protocol icmp --ingress 'allow-icmp'
 
-openstack server create --flavor m1.mini --image 'cirros-0.5.2' --network hr-network-1 --security-group default --security-group allow-ssh i1
+openstack server create --flavor m1.mini --image 'cirros-0.5.2' --network hr-network-1 \
+--security-group default --security-group allow-ssh  --security-group allow-icmp i1
 
 source admin
 openstack floating ip create --floating-ip-address '10.10.10.101' --project hr public
